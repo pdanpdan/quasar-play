c0ab21b03d1e7e719492aff0e420af62a3083133
Dan Popescu <pdan.popescu@gmail.com>
2024-01-08 15:12:14 +0200

    feat: adapt to theme changes in preview in upstream

--- src/templates/full/App.vue
+++ src/templates/full/App.vue
@@ -43,6 +43,19 @@
 </template>
 
 <style lang="sass">
+body
+  background-color: var(--play-bg-color-base)
+  color: var(--play-fg-color-base)
+
+.body
+  &--light
+    --play-bg-color-base: #f9f9f9
+    --play-fg-color-base: #111
+
+  &--dark
+    --play-bg-color-base: #111
+    --play-fg-color-base: #f9f9f9
+
 #app > .repl-container
   min-height: 100dvh
   display: flex
